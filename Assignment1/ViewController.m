fe574ea9958ab9cb78d9057b86b25f677bc2f846
whg3481 <tost2life@gmail.com>
2016-09-26 13:46:13 -0500

    Initial Commit

---
+++ Assignment1/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Assignment1
+//
+//  Created by New User on 9/26/16.
+//  Copyright © 2016 Will Garner. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ Assignment1Tests/Assignment1Tests.m
@@ -0,0 +1,39 @@
+//
+//  Assignment1Tests.m
+//  Assignment1Tests
+//
+//  Created by New User on 9/26/16.
+//  Copyright © 2016 Will Garner. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Assignment1Tests : XCTestCase
+
+@end
+
+@implementation Assignment1Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
